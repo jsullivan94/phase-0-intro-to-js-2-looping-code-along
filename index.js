96c55b50b07ff92bb53dbf55cc6adfe6e244b34b
James Sullivan <jsullivan8794@gmail.com>
2023-04-25 14:23:16 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solutions in this file
+function writeCards(names, eventname) {
+    const array = [];
+    for (let i = 0; i < names.length; i++) {
+        array.push(`Thank you, ${names[i]}, for the wonderful ${eventname} gift!`);
+       }
+
+       return array    
+
+
+    };
+    
+function countDown() {
+    let countDown = 10;
+    while (countDown >=0) {
+        console.log(countDown--)
+    }
+
+}
+
+
+
+
+
+
